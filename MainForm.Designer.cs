6ede9d04d22af56f1cf4ecb6dac6046e8b9c4b26
alexChurkin <lxchurkin@gmail.com>
2022-01-07 20:03:52 +0300

    Version incremented to 1.2.1

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -236,7 +236,7 @@
             this.Name = "MainForm";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Truck Remote Server 1.2";
+            this.Text = "Truck Remote Server 1.2.1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpPort)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.sensitivityTrackBar)).EndInit();
